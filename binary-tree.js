81dfc616ea0dee3edf4d2a14651973764c78ebe7
stevencueto <steven.cueto1305@gmail.com>
2022-04-06 01:38:43 -0400

    first working tree all over

--- binary-tree.js
+++ binary-tree.js
@@ -1,18 +1,12 @@
-// Do not edit the class below except for
-// the insert, contains, and remove methods.
-// Feel free to add new properties and methods
-// to the class.
 class BST {
-    constructor(value) {
+    constructor(value) {//to make the "root" node
         this.value = value;
         this.left = null;
         this.right = null;
     }
   
-    insert(value) {
-      // Write your code here.
-      // Do not edit the return statement of this method.
-        if(value < this.value){
+	insert(value) {
+        if(value < this.value){//checks is it has to go to le left or ro the right if is more than the root is right else is left
             if(this.left === null){
                 this.left = new BST(value)
             }else{
@@ -20,22 +14,18 @@ class BST {
             }
         }else{
             if(this.right === null){
-                this.right = new BST(value)
+                    this.right = new BST(value)
             }else{
                 this.right.insert(value)
             }
         }
-      return this;
+        return this;
     }
   
     contains(value) {
       // Write your code here.
-        if(this.value === null){
-            return false
-        }else if(this.value === value){
-            return true
-        }else if(value < this.value){
-            if (this.left == null){
+        if(value < this.value){
+            if (this.left === null){
                 return false
             }else{
                 return this.left.contains(value)
@@ -46,15 +36,42 @@ class BST {
             }else{
                 return this.right.contains(value)
             }
+        }else{
+            return true
         }
     }
   
-    remove(value) {
-      // Write your code here.
-      // Do not edit the return statement of this method.
-        if (this.value < value){
-            
-        } 
+    remove(value, parent = null) {
+        if(value < this.value){
+            if(this.left !== null){
+                this.left.remove(value, this)
+            }
+        }else if(value > this.value){
+            if(this.right !== null){
+                this.right.remove(value, this)
+            }
+        }else{
+            if(this.left !== null && this.right !== null){
+                this.value = this.right.getMinVal()
+                this.right.remove(this.value, this)
+            }else if (parent === null){
+                if(this.left !== null){
+                    this.value = this.left.value
+                    this.right = this.left.right
+                    this.left = this.left.left
+                }else if (this.right !== null){
+                    this.value = this.right.value
+                    this.left = this.right.left
+                    this.right = this.right.right
+                }else{
+                //do nothing no more nodes to look at
+                }
+            }else if (parent.left ===  this){
+                parent.left = this.left !== null ? this.left : this.right
+            }else if (parent.right === this){
+                parent.right = this.left !== null ? this.left : this.right
+            }
+         }
       return this;
     }
       
@@ -65,19 +82,26 @@ class BST {
             return this.left.getMinVal()
         }
     }
-    getMaxVal(){
-        if(this.right === null){
-            return this.value;
+    getMinVal(){
+        if(this.left === null){
+              return this.value;
         }else{
-            return this.right.getMaxVal()
+            return this.left.getMinVal()
         }
     }
 }
   
   // Do not edit the line below.
-const bts = new BST(3);
-bts.insert(3)
-bts.insert(5)
+const bts = new BST(5);
+bts.insert(6)
+bts.insert(7)
+bts.insert(8)
 bts.insert(4)
 bts.insert(3)
+bts.insert(2)
+bts.insert(1)
+bts.insert(0)
+bts.remove(6)
 console.log(bts)
+
+
